743dddc4a45a3abb09466fa30beb69e0c57474e9
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2017-10-05 10:51:45 +0300

    Dockerfilet

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || '39407e1c65185b097d80353ca7abbf8e';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
